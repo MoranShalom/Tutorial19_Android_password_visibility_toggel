c18b0b9fdb5b6d7a5e1a10c99428ba78f944392f
MoranShalom <moranshalom1234@gmail.com>
2019-04-12 01:23:32 +0300

    Android password visibility toggel

---
+++ app/src/main/java/com/example/t19_password_visibility_toggle/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.t19_password_visibility_toggle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
